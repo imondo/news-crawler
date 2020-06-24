5978d25ddef1e491c9a1e28c7aed39db9f82d154
Mondo <wuyw@sqqmall.com>
2020-06-24 16:51:16 +0800

    feat: 每天凌晨定时任务

--- utils/timeJob.js
+++ utils/timeJob.js
@@ -2,9 +2,9 @@ const schedule = require('node-schedule');
 
 /* 定时任务 */
 function timeJob (cb) {
-  // 每天上午10点执行一次
-  // 注意，这里需要加 minute:0， 否则10点的每一分钟都执行一次。
-  schedule.scheduleJob({ hour: 10, minute: 0 }, function () {
+  // 每天上午0点执行一次
+  // 注意，这里需要加 minute: 0， 否则10点的每一分钟都执行一次。
+  schedule.scheduleJob({ hour: 0, minute: 0 }, function () {
       console.log('定时任务执行一次');
       cb && cb();
   });
